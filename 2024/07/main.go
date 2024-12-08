13094a52e3dfdce5a4636d6a7c6261e81997f06e
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-12-08 10:13:08 +0200

    refactor(2024): day 07 - remove code duplication

--- 2024/07/main.go
+++ 2024/07/main.go
@@ -14,7 +14,7 @@ import (
 var input []byte
 
 /*
-* part 1:  303876485655
+* part 1: 303876485655
 * part 2: 146111650210682
  */
 func main() {
@@ -22,45 +22,38 @@ func main() {
 	log.Infof("part2: %s", solvePart2(input))
 }
 
-func f1(result int, values []int) bool {
-	if len(values) == 1 {
-		return result == values[0]
-	}
+type fn func(a, b int) int
 
-	copy := slices.Clone(values[1:])
-	copy[0] *= values[0]
-	if f1(result, copy) {
-		return true
-	}
-	copy = slices.Clone(values[1:])
-	copy[0] += values[0]
-	return f1(result, copy)
+func sum(a, b int) int {
+	return a + b
 }
 
-func f2(result int, values []int) bool {
+func mul(a, b int) int {
+	return a * b
+}
+
+func concat(a, b int) int {
+	v, _ := strconv.Atoi(strconv.Itoa(a) + strconv.Itoa(b))
+	return v
+}
+
+func f1(result int, values []int, ops []fn) bool {
 	if len(values) == 1 {
 		return result == values[0]
 	}
 
-	copy := slices.Clone(values[1:])
-	copy[0], _ = strconv.Atoi(strconv.Itoa(values[0]) + strconv.Itoa(copy[0]))
-	if f2(result, copy) {
-		return true
+	for _, op := range ops {
+		copy := slices.Clone(values[1:])
+		copy[0] = op(values[0], values[1])
+		if f1(result, copy, ops) {
+			return true
+		}
 	}
-
-	copy = slices.Clone(values[1:])
-	copy[0] *= values[0]
-	if f2(result, copy) {
-		return true
-	}
-
-	copy = slices.Clone(values[1:])
-	copy[0] += values[0]
-	return f2(result, copy)
+	return false
 }
 
 // solve
-func solvePart1(input []byte) string {
+func solvePart(input []byte, ops []fn) string {
 	output := 0
 	lines := bytes.FieldsFunc(input, func(r rune) bool { return r == '\n' })
 	for i, line := range lines {
@@ -79,38 +72,19 @@ func solvePart1(input []byte) string {
 			return v
 		})
 
-		if f1(result, values) {
+		if f1(result, values, ops) {
 			output += result
 		}
 	}
 
 	return strconv.Itoa(output)
+}
+
+func solvePart1(input []byte) string {
+	return solvePart(input, []fn{mul, sum})
 }
 
 // solve
 func solvePart2(input []byte) string {
-	output := 0
-	lines := bytes.FieldsFunc(input, func(r rune) bool { return r == '\n' })
-	for i, line := range lines {
-		log.Debugf("line %d", i)
-		data := bytes.FieldsFunc(line, func(r rune) bool { return r == ':' })
-		if len(data) != 2 {
-			return "invalid data at row"
-		}
-
-		result, err := strconv.Atoi(string(data[0]))
-		if err != nil {
-			return err.Error()
-		}
-		values := lo.Map(bytes.Fields(data[1]), func(item []byte, _ int) int {
-			v, _ := strconv.Atoi(string(item))
-			return v
-		})
-
-		if f1(result, values) || f2(result, values) {
-			output += result
-		}
-	}
-
-	return strconv.Itoa(output)
+	return solvePart(input, []fn{mul, sum, concat})
 }
