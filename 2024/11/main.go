a0635efa0798ef3ef40e5637d9aeecd3ce8c26a8
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-12-22 21:44:42 +0200

    refactor(2024): day 11
    cleanup

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -55,15 +55,7 @@ func arrangeStones(input []int, times int) int {
 }
 
 func numLen(i int) int {
-	if i == 0 {
-		return 1
-	}
-	count := 0
-	for i != 0 {
-		i /= 10
-		count++
-	}
-	return count
+	return int(math.Log10(float64(i))) + 1
 }
 
 // solve
